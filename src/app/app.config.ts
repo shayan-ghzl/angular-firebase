8babb2461120a5cb28dc047630a91389c6cb693e
Shayan Ghazali <shayan97.tti@gmail.com>
2024-11-20 13:42:30 +0330

    done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,39 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { routes } from './app.routes';
+
+const DATABASE_SHARD_URLS = [
+  'https://ssrtest-cebd9.firebaseio.com',
+  "https://ssrtest-logs.firebaseio.com/",
+  "https://ssrtest-inits.firebaseio.com/",
+];
+
+// const appName = '[DEFAULT]';
+const appName = 'APPLICATION_NAME';
+
+const glo3d_config = {
+  apiKey: "AIzaSyCRJJCgjBONQt9l7BnITY90FY1fb8N0-QU",
+  authDomain: "ssrtest-cebd9.firebaseapp.com",
+  databaseURL: "https://ssrtest-cebd9.firebaseio.com",
+  projectId: "ssrtest-cebd9",
+  storageBucket: "ssrtest-cebd9.appspot.com",
+  messagingSenderId: "1059547878701",
+  appId: "1:1059547878701:web:7ea4c2029a902c93f26fa4",
+  measurementId: "G-47MBGH17R7"
+};
+
+const my_config = {
+  projectId: "first-firebase-project-904e5",
+  appId: "1:940644286738:web:f3f5bcdf8594233a4e73cb",
+  storageBucket: "first-firebase-project-904e5.firebasestorage.app",
+  apiKey: "AIzaSyCXXI5cFlgj6vJWuze6zzP56qA7AIA2Qhc",
+  authDomain: "first-firebase-project-904e5.firebaseapp.com",
+  messagingSenderId: "940644286738"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,13 +41,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     // needed in any firebase application
-    provideFirebaseApp(() => initializeApp({
-      "projectId": "first-firebase-project-904e5",
-      "appId": "1:940644286738:web:f3f5bcdf8594233a4e73cb",
-      "storageBucket": "first-firebase-project-904e5.firebasestorage.app",
-      "apiKey": "AIzaSyCXXI5cFlgj6vJWuze6zzP56qA7AIA2Qhc",
-      "authDomain": "first-firebase-project-904e5.firebaseapp.com",
-      "messagingSenderId": "940644286738"
-    })),
+    provideFirebaseApp(() => initializeApp(my_config, appName)),
+    // needed when you need realtime databse
+    // provideDatabase(() => getDatabase()),
+    provideDatabase(() => getDatabase(getApp(appName))),
+    // provideDatabase(injector => getDatabase(getApp(appName), DATABASE_SHARD_URLS[0])),
+    // provideDatabase(injector => getDatabase(injector.get(FirebaseApp), DATABASE_SHARD_URLS[0])),
+    // provideDatabase(injector => getDatabase(injector.get(FirebaseApp), DATABASE_SHARD_URLS[1])),
+    // provideDatabase(injector => getDatabase(injector.get(FirebaseApp), DATABASE_SHARD_URLS[2])),
   ]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Database, equalTo, get, listVal, objectVal, orderByChild, query, ref } from '@angular/fire/database';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-  ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  formGroup = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    family: new FormControl('', [Validators.required]),
-  });
 
-  addFriend(): void {
+    private database = inject(Database);
+    // private databaseInstances = inject(DatabaseInstances);
+    // private firebaseApp = inject(FirebaseApp);
 
-  }
+    formGroup = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        family: new FormControl('', [Validators.required]),
+    });
+
+    ngOnInit(): void {
+        // console.log(this.database.app === this.firebaseApp);
+        // this.databaseInstances.forEach(database => {
+        //   console.log(database.app === this.firebaseApp);
+        //   debugger;
+        // });
+
+        // --------------------------------------------
+        const myRootDataRef = ref(this.database, '/');
+        const myCoursesDataRef = ref(this.database, '/courses');
+        const myFriendsDataRef = ref(this.database, '/friends');
+        // --------------------------------------------
+        get(myFriendsDataRef).then(snapshot => {
+            const result = snapshot.val();
+
+            console.log('Fetched data:', result);
+            console.log('Fetched data:', Object.values(result));
+            debugger;
+        });
+        // --------------------------------------------
+        // To fetch specific data, you can use query constraints (e.g., sorting, filtering).
+
+        // important:
+        // Firebase Realtime Database uses indexing to optimize query performance.
+        // Without it, Firebase cannot perform efficient lookups for fields like family, resulting in the error.;
+        // {
+        //     "rules": {
+        //         ".read": "now < 1734640200000",  // 2024-12-20
+        //         ".write": "now < 1734640200000",  // 2024-12-20
+        //         "friends": {
+        //             ".indexOn": ["family"];   // Add the "family" index
+        //         }
+        //     }
+        // }
+
+        const ghazaliQuery = query(myFriendsDataRef, orderByChild('family'), equalTo('ghazali'));
+        get(ghazaliQuery).then(snapshot => {
+            const result = snapshot.val();
+
+            console.log('Fetched data:', result);
+            debugger;
+        });
+
+        // it is real time changes
+        // it will emit a list
+        (listVal(ghazaliQuery) as Observable<any[]>).subscribe(response => {
+            debugger;
+        });
+        // --------------------------------------------
+        // it is real time changes
+        // it will emit an object
+        (objectVal(myCoursesDataRef) as Observable<any[]>).subscribe(response => {
+            debugger;
+        });
+    }
+
+    addFriend(): void {
+
+    }
 }
